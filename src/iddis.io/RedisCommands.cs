e994c14de56af73cc03c8b8fb423446321b49c8a
rooseveltdossantos <rooseveltdossantos@hotmail.com>
2014-04-05 11:21:08 -0300

    Commit inicial

---
+++ src/iddis.io/RedisCommands.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace iddis.io
+{
+    public class RedisCommands
+    {
+        private static readonly string CRLF = new string(new[] {'\r', '\n'});
+        private static readonly int CRLFLength = CRLF.Length;
+        private const  byte DATA_TYPE_BYTE_LENGTH = 1;
+
+        private static char TYPE_SIMPLE_STRINGS = '+';
+        private static char TYPE_ERRORS = '-';
+        private static char TYPE_INTEGERS = ':';
+        private const char TYPE_BULK_STRINGS = '$';
+        private static char TYPE_ARRAYS = '*';
+
+        public static string LLEN(string key)
+        {
+            return BulkString(iddisio.CMD_LLEN, key);
+        }
+
+        public static string SET(string key, string value)
+        {
+            return BulkString(iddisio.CMD_SET, key, value);
+        }
+        
+        public static string SET(string key, string value, NXXX nxxx)
+        {
+            return BulkString(iddisio.CMD_SET, key, value, nxxx.ToString());
+        }
+
+        public static string SET(string key, string value, int seconds)
+        {
+            return BulkString(iddisio.CMD_SET, key, value, iddisio.PAR_SET_EX, seconds.ToString());
+        }
+
+        public static string SET(string key, string value, int seconds, NXXX nxxx)
+        {
+            return BulkString(iddisio.CMD_SET, key, value, iddisio.PAR_SET_EX, seconds.ToString(), nxxx.ToString());
+        }
+
+        public static string SET(string key, string value, int seconds, int miliseconds)
+        {
+            return BulkString(iddisio.CMD_SET, key, value, iddisio.PAR_SET_EX, seconds.ToString(), iddisio.PAR_SET_PX, miliseconds.ToString());
+        }
+
+        public static string SET(string key, string value, int seconds, int miliseconds, NXXX nxxxx)
+        {
+            return BulkString(iddisio.CMD_SET, key, value, iddisio.PAR_SET_EX, seconds.ToString(), iddisio.PAR_SET_PX, miliseconds.ToString(), nxxxx.ToString());
+        }
+
+        private static string BulkString(params string[] strings)
+        {
+            var size = strings.Select(s => DATA_TYPE_BYTE_LENGTH + ((int) (Math.Log10(s.Length)) + 1) + CRLFLength + s.Length + CRLFLength).Sum();
+            var sb = new StringBuilder(size, size);
+            Array.ForEach(strings, s => sb.AppendFormat("{0}{1}{2}{3}{4}", TYPE_BULK_STRINGS, s.Length, CRLF, s, CRLF));
+            return sb.ToString();
+        }
+    }
+
+    public enum NXXX
+    {
+        NX = 0,
+        XX = 1
+    }
+}
---
+++ tests/iddis.io.tests/Redis/RedisCommandsTest.cs
@@ -0,0 +1,34 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace iddis.io.tests.Redis
+{
+    [TestFixture]
+    public class RedisCommandsTest
+    {
+        [Test]
+        public void should_get_llen_protocol()
+        {
+            RedisCommands.LLEN("mylist").Should().Be("$4\r\nLLEN\r\n$6\r\nmylist\r\n");
+        }
+
+        [Test]
+        public void should_get_set_protocol()
+        {
+            RedisCommands.SET("key", "value").Should().Be("$3\r\nSET\r\n$3\r\nkey\r\n$5\r\nvalue\r\n");
+            
+            RedisCommands.SET("key", "value", 1000).Should().Be("$3\r\nSET\r\n$3\r\nkey\r\n$5\r\nvalue\r\n$2\r\nEX\r\n$4\r\n1000\r\n");
+            
+            RedisCommands.SET("key", "value", 2000, 2000).Should().Be("$3\r\nSET\r\n$3\r\nkey\r\n$5\r\nvalue\r\n$2\r\nEX\r\n$4\r\n2000\r\n$2\r\nPX\r\n$4\r\n2000\r\n");
+            
+            RedisCommands.SET("key", "value", 2000, 2000, NXXX.NX).Should().Be("$3\r\nSET\r\n$3\r\nkey\r\n$5\r\nvalue\r\n$2\r\nEX\r\n$4\r\n2000\r\n$2\r\nPX\r\n$4\r\n2000\r\n$2\r\nNX\r\n");
+            RedisCommands.SET("key", "value", 2000, 2000, NXXX.XX).Should().Be("$3\r\nSET\r\n$3\r\nkey\r\n$5\r\nvalue\r\n$2\r\nEX\r\n$4\r\n2000\r\n$2\r\nPX\r\n$4\r\n2000\r\n$2\r\nXX\r\n");
+            
+            RedisCommands.SET("key", "value", NXXX.NX).Should().Be("$3\r\nSET\r\n$3\r\nkey\r\n$5\r\nvalue\r\n$2\r\nNX\r\n");
+            RedisCommands.SET("key", "value", NXXX.XX).Should().Be("$3\r\nSET\r\n$3\r\nkey\r\n$5\r\nvalue\r\n$2\r\nXX\r\n");
+            
+            RedisCommands.SET("key", "value", 1000, NXXX.NX).Should().Be("$3\r\nSET\r\n$3\r\nkey\r\n$5\r\nvalue\r\n$2\r\nEX\r\n$4\r\n1000\r\n$2\r\nNX\r\n");
+            RedisCommands.SET("key", "value", 1000, NXXX.XX).Should().Be("$3\r\nSET\r\n$3\r\nkey\r\n$5\r\nvalue\r\n$2\r\nEX\r\n$4\r\n1000\r\n$2\r\nXX\r\n");
+        }
+    }
+}
