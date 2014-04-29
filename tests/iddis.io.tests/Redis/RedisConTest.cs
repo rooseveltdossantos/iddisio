0eaad625d83dcca505e655c4ecb7b5ae1ef26730
rooseveltdossantos <rooseveltdossantos@hotmail.com>
2014-04-28 22:12:21 -0300

    Testes iniciais de comunicação com o REDIS Server.

---
+++ tests/iddis.io.tests/Redis/RedisConTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using System.Net.Sockets;
+using NUnit.Framework;
+
+namespace iddis.io.tests.Redis
+{
+    [TestFixture]
+    public class RedisConTest
+    {
+        [Test]
+        public void should_send_command()
+        {
+            var buffer = new byte[500];
+            var setProtocol = RedisCommands.SET("key", "value").Select(c => (byte)c).ToArray();
+            var s = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            s.Connect("localhost", 6379);
+            var send = s.Send(setProtocol);
+            s.Receive(buffer);
+            foreach (var b in buffer)
+                Console.Write((char) b);
+            s.Close();
+        }
+    }
+}
