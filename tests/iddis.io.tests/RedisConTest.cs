af3bf78e58b9f9c2184b43e3772114bcef27ee92
rooseveltdossantos <rooseveltdossantos@hotmail.com>
2014-05-27 21:22:42 -0300

    Work in progress

--- tests/iddis.io.tests/RedisConTest.cs
+++ tests/iddis.io.tests/RedisConTest.cs
@@ -9,31 +9,34 @@ namespace iddis.io.tests
         [Test]
         public void should_send_command()
         {
-            var redisCon = new RedisCon("localhost", 6379);
+            var redisCommands = new RedisCommands(new RedisBuffer(1024));
+            var redisCon = new RedisCon("localhost", 6379, redisCommands);
             redisCon.SET("key", "value");
         }
     }
 
     public class RedisCon
     {
+        private readonly RedisCommands redisCommands;
         public string Host { get; private set; }
         public int Port { get; private set; }
         private readonly Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
 
-        public RedisCon(string host, int port)
+        public RedisCon(string host, int port, RedisCommands redisCommands)
         {
+            this.redisCommands = redisCommands;
             Host = host;
             Port = port;
         }
 
         public void SET(string key, string value)
         {
-            var setProtocol = RedisCommands.SET(key, value);
+            var setProtocol = redisCommands.SET(key, value);
             socket.Connect(Host, Port);
             try
             {
-                //TODO:Rever
-                socket.Send(setProtocol.buffer);
+                //TODO:Review
+                socket.Send(setProtocol.GetRaw());
             }
             finally
             {
